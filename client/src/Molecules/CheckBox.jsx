c69187b97ec26a9cb387d2600ceab3687841ea24
Tkr-karan <karankumar072508@gmail.com>
2023-11-13 17:50:58 +0530

    survey block fe setup

--- client/src/Molecules/CheckBox.jsx
+++ client/src/Molecules/CheckBox.jsx
@@ -1,36 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const CheckBox = () => {
-  const handleFocus = (e) => {
-    e.target.textContent = " ";
-    e.target.contentEditable = true;
+export const CheckBox = ({ getData, index }) => {
+  const [question, setQuestion] = useState({
+    description: "",
+    options: [
+      { id: 1, text: "" },
+      { id: 2, text: "" },
+    ],
+  });
+
+  const handleQuestionChange = (e) => {
+    setQuestion({ ...question, description: e.target.value });
+    updateData();
+  };
+
+  const handleOptionChange = (optionId, newText) => {
+    const updatedOptions = question.options.map((option) =>
+      option.id === optionId ? { ...option, text: newText } : option
+    );
+    setQuestion({ ...question, options: updatedOptions });
+    updateData();
+  };
+
+  //   const handleDataChange = (optionId, newText) => {
+  //     const updatedOptions = options.map((option) =>
+  //       option.id === optionId ? { ...option, text: newText } : option
+  //     );
+  //     setOptions(updatedOptions);
+
+  //     const data = updatedOptions.map((option) => option.text);
+  //     getData(data);
+  //   };
+
+  const updateData = () => {
+    const data = {
+      description: question.description,
+      options: question.options.map((option) => option.text),
+    };
+    getData(data);
   };
 
   return (
     <div>
-      <p contentEditable="true">Select the checkbox questions</p>
-      <div className=" flex">
-        <input type="checkbox" />{" "}
-        <p
-          className="w-10rem"
-          contentEditable="true"
-          onFocus={(e) => handleFocus(e)}
-        >
-          here you enter option
-        </p>
+      <div>
+        <label>
+          <input
+            type="text"
+            value={question.description}
+            onChange={handleQuestionChange}
+            placeholder="Select the checkbox questions"
+          />
+        </label>
       </div>
-      <div className="flex">
-        <input type="checkbox" />{" "}
-        <p contentEditable="true">here you enter option</p>
-      </div>
-      <div className="flex">
-        <input type="checkbox" />{" "}
-        <p contentEditable="true">here you enter option</p>
-      </div>
-      <div className="flex">
-        <input type="checkbox" />{" "}
-        <p contentEditable="true">here you enter option</p>
-      </div>
+      {/* <p contentEditable="true" onInput={handleQuestionChange}>
+        {question.description || "Select the checkbox questions"}
+      </p> */}
+      {question.options.map((option) => (
+        <div className="flex" key={option.id}>
+          <input type="checkbox" />{" "}
+          <input
+            className="w-10rem"
+            type="text"
+            // onFocus={(e) => handleFocus(e)}
+            onChange={(e) => handleOptionChange(option.id, e.target.value)}
+            value={option.text}
+            placeholder="option value"
+          />
+        </div>
+      ))}
     </div>
   );
 };
--- client/src/Molecules/RadioFields.jsx
+++ client/src/Molecules/RadioFields.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const RadioFields = () => {
+export const RadioFields = ({ getData }) => {
+  const [question, setQuestion] = useState({
+    description: "",
+    options: [
+      { id: 1, text: "Yes" },
+      { id: 2, text: "No" },
+    ],
+  });
+
+  const handleQuestionChange = (e) => {
+    setQuestion({ ...question, description: e.target.value });
+    updateData();
+  };
+  const handleOptionChange = (optionId, newText) => {
+    const updatedOptions = question.options.map((option) =>
+      option.id === optionId ? { ...option, text: newText } : option
+    );
+    setQuestion({ ...question, options: updatedOptions });
+    updateData();
+  };
+
+  const updateData = () => {
+    const data = {
+      description: question.description,
+      options: question.options.map((option) => option.text),
+    };
+    getData(data);
+  };
+
   return (
     <div>
-      <p contentEditable="true">Select the radio questions</p>
-      <input type="radio" contentEditable="true" /> Yes
-      <input type="radio" contentEditable="true" /> No
+      <div>
+        <label>
+          <input
+            type="text"
+            value={question.description}
+            onChange={handleQuestionChange}
+            placeholder="Select the radio questions"
+          />
+        </label>
+      </div>
+      {question.options.map((option) => (
+        <div className="flex" key={option.id}>
+          <input type="radio" />{" "}
+          <input
+            className="w-10rem"
+            type="text"
+            // onFocus={(e) => handleFocus(e)}
+            onChange={(e) => handleOptionChange(option.id, e.target.value)}
+            value={option.text}
+            placeholder="option value"
+          />
+        </div>
+      ))}
     </div>
   );
 };
