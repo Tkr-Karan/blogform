7381121b276f5c2a1788fc792cd854e13adc3bf9
Tkr-karan <karankumar072508@gmail.com>
2023-11-13 17:52:16 +0530

    setting up the router & model for the survey block

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const ImageRoutes = require("./routes/ImageBlockRoutes");
 const VideoRoutes = require("./routes/VideoBlockRoutes");
 const PublishedRoutes = require("./routes/PublishedBlocksRoutes");
 const AnalyticRoutes = require("./routes/AnalyticsRoutes");
+const SurveyRoutes = require('./routes/SurveyRoutes')
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +30,7 @@ app.use("/api/image", ImageRoutes);
 app.use("/api/video", VideoRoutes);
 app.use("/api/published", PublishedRoutes);
 app.use("/api/analytics", AnalyticRoutes);
+app.use('/api/survey', SurveyRoutes)
 
 app.listen(PORT, () => {
   console.log(`Your server is working fine on ${PORT}, keep working`);
