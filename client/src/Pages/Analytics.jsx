8f0565fdcc5fe7449a718c66f1bfa41f14da4101
Tkr-karan <karankumar072508@gmail.com>
2023-11-20 09:36:16 +0530

    code refactoring

--- client/src/Pages/Analytics.jsx
+++ client/src/Pages/Analytics.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
 import Loader from "../Atom/Loader";
+import "./Stylesheets/Analytics.css";
 
 const Analytics = () => {
   const [analyticData, setAnalyticData] = useState(null);
+  const [searchBlock, setSearchBlock] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,21 +21,63 @@ const Analytics = () => {
     navigate(`/blocks/${id}`);
   };
 
+  const handleSearch = (e) => {
+    setSearchBlock(e.target.value);
+  };
+
+  const filteredBlocks = analyticData
+    ? Object.keys(analyticData).filter((block) =>
+        analyticData[block].title
+          .toLowerCase()
+          .includes(searchBlock.toLowerCase())
+      )
+    : [];
+
   return (
-    <div className="w-[100%] h-[100%] flex justify-center items-center">
+    <div className="w-[100%] h-[100%] flex gap-6 justify-center items-center add-img">
       {analyticData ? (
-        <div className="flex flex-wrap gap-2 w-[90%]">
-          {Object.keys(analyticData).map((block) => {
-            return (
-              <div
-                className="bg-orange-400/30 p-2 mt-1 rounded-lg cursor-pointer"
-                key={analyticData[block]._id}
-                onClick={() => goToTestBlock(analyticData[block]._id)}
-              >
-                {analyticData[block].title}
-              </div>
-            );
-          })}
+        <div
+          className="flex flex-col items-center justify-start
+         gap-8 h-[100%]"
+        >
+          <div className="search w-[50rem] h-[2.5rem] flex items-center bg-white  rounded-md overflow-hidden p-2  mt-10">
+            <input
+              className="w-[100%] h-[100%] p-2 border-none outline-none "
+              type="text"
+              placeholder="use me for searching blocks"
+              value={searchBlock}
+              onChange={(e) => handleSearch(e)}
+            />
+            <i className="fa-solid fa-magnifying-glass cursor-pointer"></i>
+          </div>
+
+          {searchBlock.length > 3 ? (
+            <div className="flex flex-wrap gap-2 w-[90%]  h-[40%] justify-center items-center ">
+              {filteredBlocks.map((block) => (
+                <div
+                  className="bg-orange-400/100 p-2 mt-1 rounded-lg cursor-pointer h-[3rem] flex justify-center items-center"
+                  key={analyticData[block]._id}
+                  onClick={() => goToTestBlock(analyticData[block]._id)}
+                >
+                  {analyticData[block].title}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-wrap gap-2 w-[90%] h-[80%] mb-4 overflow-y-scroll items-start justify-center">
+              {Object.keys(analyticData).map((block) => {
+                return (
+                  <div
+                    className="bg-orange-400/100 p-2 mt-1 rounded-lg cursor-pointer h-[3rem] flex justify-center items-center"
+                    key={analyticData[block]._id}
+                    onClick={() => goToTestBlock(analyticData[block]._id)}
+                  >
+                    {analyticData[block].title}
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       ) : (
         <Loader />
--- client/src/components/VideoImageSurvey.jsx
+++ client/src/components/VideoImageSurvey.jsx
@@ -3,67 +3,85 @@ import Loader from "../Atom/Loader";
 export default function VideoImageSurvey(props) {
   const { blockData, handleClick } = props;
   return (
-    <div style={{ marginLeft: "3rem", width: "80%" }}>
+    <div
+      style={{
+        marginLeft: "3rem",
+        width: "80%",
+        alignSelf: "baseline",
+        height: "100%",
+      }}
+    >
       {blockData.imageData.data &&
       blockData.videoData.data &&
       blockData.surveyData.data ? (
-        <div className="w-[100%] h-[100%] flex flex-col gap-4 align-center justify-center">
-          <div className="dashboard-heading">
-            <h1 className="text-[1.5rem] font-bold">Welcome To Dashboard!!</h1>
-          </div>
-          <div className="w-[100%] flex flex-wrap gap-4 self-center items-center justify-center">
-            <div className=" flex gap-2" style={{ width: "100%" }}>
-              <h2 className="text-2xl capitalize">images</h2>
-              <div className=" w-[90%] flex flex-wrap gap-1 justify-start items-center">
-                {blockData.imageData.data.map((block, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="w-[8rem] h-[5rem]  p-2 rounded-lg bg-slate-400 cursor-pointer flex justify-center items-center"
-                      onClick={() => handleClick(block, "image")}
-                    >
-                      {block.title}
-                    </div>
-                  );
-                })}
+        <div className="w-[100%] h-[100%] flex flex-col gap-4 align-center justify-start">
+          {blockData.length < 1 ? (
+            <div className="dashboard-heading">
+              <h1 className="text-[1.5rem] font-bold">
+                Welcome To Dashboard!!
+              </h1>
+            </div>
+          ) : (
+            <div className="w-[100%] grid grid-cols-3 flex-wrap gap-4 self-center items-start justify-center mt-4">
+              <div
+                className=" flex flex-col items-center gap-2"
+                style={{ width: "100%" }}
+              >
+                <h2 className="text-2xl capitalize">images</h2>
+                <div className=" w-[90%] flex flex-wrap gap-1 justify-start items-center  h-[80%]  overflow-y-scroll">
+                  {blockData.imageData.data.map((block, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="w-[85%] h-[5rem]  p-2 rounded-lg bg-slate-400 cursor-pointer flex justify-center items-center"
+                        onClick={() => handleClick(block, "image")}
+                      >
+                        {block.title}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 w-[100%] h-[100%] ">
+                <h2 className="text-2xl capitalize">videos</h2>
+                <div className=" w-[90%] flex flex-wrap gap-1 justify-center items-center h-[80%]  overflow-y-scroll ">
+                  {blockData.videoData.data.map((block, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="w-[85%] h-[5rem] p-2 rounded-lg bg-violet-400 cursor-pointer flex justify-center items-center"
+                        onClick={() => handleClick(block)}
+                      >
+                        {block.title}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 w-[100%]">
+                <h2 className="text-2xl capitalize">Survey</h2>
+                <div className=" w-[90%] flex flex-col gap-1 justify-start items-center  h-[80%]  overflow-y-scroll">
+                  {blockData.surveyData.data.map((block, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="w-[85%] h-[5rem] p-2 rounded-lg bg-red-200 cursor-pointer flex justify-center items-center"
+                        onClick={() => handleClick(block, "survey")}
+                      >
+                        {block.title}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            <div className="flex gap-2 w-[100%]">
-              <h2 className="text-2xl capitalize">videos</h2>
-              <div className=" w-[90%] flex flex-wrap gap-1 justify-start items-center">
-                {blockData.videoData.data.map((block, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="w-[8rem] h-[5rem] p-2 rounded-lg bg-violet-400 cursor-pointer flex justify-center items-center"
-                      onClick={() => handleClick(block)}
-                    >
-                      {block.title}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div className="flex gap-2 w-[100%]">
-              <h2 className="text-2xl capitalize">Survey</h2>
-              <div className=" w-[90%] flex flex-wrap gap-1 justify-start items-center">
-                {blockData.surveyData.data.map((block, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="w-[8rem] h-[5rem] p-2 rounded-lg bg-red-200 cursor-pointer flex justify-center items-center"
-                      onClick={() => handleClick(block, "survey")}
-                    >
-                      {block.title}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
+          )}
         </div>
       ) : (
-        <Loader />
+        <div className="w-[100%] h-[100%] flex justify-center items-center">
+          <Loader />
+
+        </div>
       )}
     </div>
   );
